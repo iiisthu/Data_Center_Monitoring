eff654d724e3c6eea1c0e19f7111c0a3b4cd5055
S.Wang <wsh@physics.so>
2014-07-05 20:51:44 +0800

    Fixed some bugs
    Now the program is ready to rock!

--- draw.m
+++ draw.m
@@ -29,31 +29,33 @@ for l=2:leno
     enddateline = str2num(strcat(endyear,endmonth,endday));
     endtimeline = str2num(strcat(endhour,endminute,endsecond));
     surname = cell2mat(operations(l,6));
-    directory11 = dir('.\1.1zuo\');
-    mkdir('.\1.1zuo\figs');
+    directory11 = dir('.\sensor\');
+    mkdir('.\sensor\figs');
     % For every txt file, get the values we want by the above parameters
     % and plot them out.
     for i = 1:length(directory11)
-        data=[];
+        date=[];
         time=[];
         temp=[];
         database=[];
         ini=1;
         fin=1;
+        mn=0;
         len = length(directory11(i).name);
         name = strcat(directory11(i).name,'');
         if len>=4
             if length(strfind(name(len-3:len),'.txt')) == 1
-                [date,time,temp]=textread(strcat('.\1.1zuo\',name),'%s %s\t%s');
+                [date,time,temp]=textread(strcat('.\sensor\',name),'%s %s\t%s');
                 database=[date time temp];
                 ini=1;
-                fin=length(database);
+                fin=1;
                 for m = 1:length(database)
                     datadate = cell2mat(date(m));
+                    datatime = cell2mat(time(m));
+                    if length(datadate) == 10 && length(datatime) == 8
                     datayear = datadate(1:4);
                     datamonth = datadate(6:7);
                     dataday = datadate(9:10);
-                    datatime = cell2mat(time(m));
                     datahour = datatime(1:2);
                     dataminute = datatime(4:5);
                     datasecond = datatime(7:8);
@@ -63,13 +65,17 @@ for l=2:leno
                         ini = m;
                         break;
                     end
+                    end
                 end
+                datadate=[];
+                datatime=[];
                 for n = 1:length(database)
                     datadate = cell2mat(date(n));
+                    datatime = cell2mat(time(n));
+                    if length(datadate) == 10 && length(datatime) == 8
                     datayear = datadate(1:4);
                     datamonth = datadate(6:7);
                     dataday = datadate(9:10);
-                    datatime = cell2mat(time(n));
                     datahour = datatime(1:2);
                     dataminute = datatime(4:5);
                     datasecond = datatime(7:8);
@@ -79,13 +85,22 @@ for l=2:leno
                         fin = n;
                         break;
                     end
+                    end
                 end
-                if ini == fin
+                if ini >= fin
                     continue;
                 end
+                %ini
+                %fin
                 k=ini:fin;
-                plot(k,cellfun(@str2num,database(ini:fin,3)));
-                saveas(gcf,strcat('.\1.1zuo\figs\',surname,'_',directory11(i).name(1:len-4),'_plot.jpg')); % Save the fig in ./figs/
+                mn = mean(cellfun(@str2num,database(ini:fin,3)));
+                hold on;
+                grid on;
+                plot(k,cellfun(@str2num,database(ini:fin,3)),'g*');
+                plot(k,mn,'rd');
+                title(num2str(mn));
+                hold off;
+                print(gcf,'-dpng',strcat('.\sensor\figs\',surname,'_',directory11(i).name(1:len-4),'_plot.png')); % Save the fig in ./figs/
                 close;
             end
         end
