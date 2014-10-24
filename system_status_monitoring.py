3fa1cb3fb7f79b856e8286a522ed56726e88004f
S.Wang <wsh@physics.so>
2014-10-24 17:22:15 +0800

    Iteration in EXACTLY every five minutes

--- system_status_monitoring.py
+++ system_status_monitoring.py
@@ -3,13 +3,15 @@
 
 import psutil,datetime,time,socket
 
-# Start iteration in every five minutes.
-
-while 1:
+def collectStatus():
 
     second = 5 # Time tick for counting.
-    interval = 300 # Time interval between collections.
 
+    # Get date and time
+    
+    time_on_written = datetime.datetime.now()
+    date_time = time_on_written.strftime('%Y-%m-%d %H:%M:%S')
+    
     # Get IP address.
 
     IP = socket.gethostbyname(socket.gethostname())
@@ -24,39 +26,42 @@ while 1:
     physical_memory_usage = psutil.virtual_memory()[2]
 
     # Hard disk usage: data input and data output among all the disks (bytes/second).
-
+    # Network usage: data transfered in and out (bytes/second).
+    
     input_before_sleep = psutil.disk_io_counters()[2]
     output_before_sleep = psutil.disk_io_counters()[3]
+    in_before_sleep = psutil.net_io_counters()[0]
+    out_before_sleep = psutil.net_io_counters()[1]
     time.sleep(second)
     input_after_sleep = psutil.disk_io_counters()[2]
     output_after_sleep = psutil.disk_io_counters()[3]
+    in_after_sleep = psutil.net_io_counters()[0]
+    out_after_sleep = psutil.net_io_counters()[1]
 
     data_input = (input_after_sleep - input_before_sleep)/5
     data_output = (output_after_sleep - output_before_sleep)/5
 
-    # Network usage: data transfered in and out (bytes/second).
-
-    in_before_sleep = psutil.net_io_counters()[0]
-    out_before_sleep = psutil.net_io_counters()[1]
-    time.sleep(second)
-    in_after_sleep = psutil.net_io_counters()[0]
-    out_after_sleep = psutil.net_io_counters()[1]
-
     net_in = (in_after_sleep - in_before_sleep)/5
     net_out = (out_after_sleep - out_before_sleep)/5
-
-    # Get date and time
-
-    date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # Write the status data into a txt file (BY APPENDING!)
 
     status = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' %(str(IP),date_time, str(cpu_usage), str(swap_memory_usage), str(physical_memory_usage), str(data_input), str(data_output), str(net_in), str(net_out))
 
-    output = open('/home/lihu/monitor/status.txt','a')
+    output = open('status.txt','a')
     output.write(str(status))
     output.close()
 
-    # Wait for five minutes until the next collection.
+    return time_on_written
 
-    time.sleep(interval)
+# Start iteration in EXACTLY every five minutes.
+if __name__ == "__main__":
+
+    global time_now
+    time_now = datetime.datetime.now()
+    
+    while 1:
+        if (datetime.datetime.now()- time_now).seconds >= 300:
+            time_now = collectStatus()
+        else:
+            time.sleep(1)
