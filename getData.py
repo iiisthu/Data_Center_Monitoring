3dda9c0edc9cac394d3857138c0ca914004c63b3
S.Wang <wsh@physics.so>
2014-10-29 23:13:37 +0800

    Fetching data from MySQL for further analysis.

---
+++ getData.py
@@ -0,0 +1,20 @@
+import MySQLdb
+
+sql = 'select * from status'
+try:
+    conn=MySQLdb.connect(host='10.1.0.5',user='root',passwd='root123',db='monitor',port=3306)
+    cur=conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    output = open('data.txt','a')
+    for row in rows:
+        status = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' %(row[0],row[1].strftime('%Y-%m-%d %H:%M:%S'), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]))
+        output.write(str(status))
+    output.close()
+    cur.close()
+    conn.close()
+except MySQLdb.Error,e:
+    output = open('status.txt','a')
+    output.write("\nSQL: %s\n" % (sql))
+    output.write("Mysql Error %d: %s\n" % (e.args[0], e.args[1]))
+    output.close()
